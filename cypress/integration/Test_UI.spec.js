b84ffe9314b398495f6f152d3428514ef9fac0f1
hedmon84 <hedmonlopes@gmail.com>
2021-07-06 10:14:35 -0600

    Date Time

--- cypress/integration/Test_UI.spec.js
+++ cypress/integration/Test_UI.spec.js
@@ -58,7 +58,7 @@ describe("Tests is only to check if calendar , dropdown, radiobutton works", () 
       .should("contain", "1:00");
   });
 
-  it("Testing  Radio Button", () => {
+  it("Testing  Radio Button Starting Date", () => {
     cy.get(
       "body > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > input[type=radio]:nth-child(4)"
     )
@@ -85,5 +85,33 @@ describe("Tests is only to check if calendar , dropdown, radiobutton works", () 
       .type("2:00")
       .invoke("prop", "value")
       .should("contain", "2:00");
+  });
+
+  it("Testing  Radio Button Leaving Date", () => {
+    cy.get(
+      "body > form > table > tbody > tr:nth-child(3) > td:nth-child(2) > input[type=radio]:nth-child(4)"
+    ).check({ force: true });
+    cy.get(
+      "body > form > table > tbody > tr:nth-child(3) > td:nth-child(2) > input[type=radio]:nth-child(5)"
+    ).check({ force: true });
+    cy.get("form").submit();
+  });
+
+  it("Checking Correct Parking Costs", () => {
+    cy.get(
+      "body > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > span.SubHead > b"
+    ).then((price) => {
+      const value = price.text();
+      expect(value).to.equal("$ 2.00");
+    });
+  });
+
+  it("Checking Correct Date", () => {
+    cy.get(
+      "body > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > span.BodyCopy > b"
+    ).then((date) => {
+      const day = date.text().trim();
+      expect(day).to.equal("(0 Days, 1 Hours, 0 Minutes)");
+    });
   });
 });
